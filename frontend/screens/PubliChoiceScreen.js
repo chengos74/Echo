a2bb62b84842520f4a1b59bde8d9eb3ac9e64c43
Gauthier Leclair <gauthierleclair1@outlook.fr>
2022-08-11 17:36:37 +0200

    fix galery not working

--- frontend/screens/PubliChoiceScreen.js
+++ frontend/screens/PubliChoiceScreen.js
@@ -35,11 +35,67 @@ function PubliChoice(props) {
 			return;
 		};
 		// assigner l'uri de l'image choisie à la state d'état
-		if(result !== null){
+		if (result !== null) {
 			setSelectedImage({ localUri: result.uri });
 		}
 	};
+	if (selectedImage !== null) {
+		return (
+			<View style={{ backgroundColor: '#151515', flex: 1 }}>
+				<View style={{ flexDirection: 'row', marginTop: 50, justifyContent: 'space-between', alignItems: 'center' }}>
+					<TouchableOpacity
+						style={{ marginLeft: 10 }}
+						title='Back'
+						onPress={() => { props.navigation.navigate("Home", { screen: "Home" }); }}
+					>
+						<FontAwesomeIcon icon={faCircleXmark} size={35} color={'white'} />
+					</TouchableOpacity>
+					<Text style={{ fontWeight: 'bold', color: 'white', fontSize: 20 }}>New publication </Text>
+					<TouchableOpacity
+						style={styles.square}
+						title='Back'
+						onPress={() => { props.navigation.navigate("PParams", { screen: "PParams" }); }}
+					>
+						<Text style={{ color: '#67D692', fontWeight: 'bold', marginRight: 10, fontSize: 17 }}>Next</Text>
+					</TouchableOpacity>
+				</View>
 
+				<View style={{ alignItems: 'center', paddingTop: 20 }}>
+					<View style={{ alignItems: 'center', }}>
+						<Image source={{ uri: selectedImage.localUri }} style={{ width: 320, height: 240, borderRadius: 20 }} />
+					</View>
+				</View>
+
+				<View style={{ alignItems: 'center', flexDirection: 'row', marginLeft: 20, }}>
+					<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+
+						{/* Bouton Pellicule */}
+						<TouchableOpacity
+							hitSlop={{ top: 30, bottom: 30, left: 30, right: 30 }}
+							style={{ marginRight: 10, }}
+							title='Pellicule'
+							onPress={() => { pickImage() }}
+						>
+							<FontAwesomeIcon icon={faImages} size={30} color={'#fff'} />
+						</TouchableOpacity>
+
+						{/* Bouton Camera */}
+						<Text style={{ color: 'white', fontWeight: 'bold', fontSize: 24, marginRight: 10 }}>/</Text>
+						<TouchableOpacity
+							hitSlop={{ top: 30, bottom: 30, left: 30, right: 30 }}
+							title='Camera'
+							onPress={() => { props.navigation.navigate("CameraScreen", { screen: "CameraScreen" }); }}
+						>
+							<FontAwesomeIcon icon={faCamera} size={30} color={'#fff'} />
+						</TouchableOpacity>
+
+					</View>
+					<View style={{ alignItems: 'center', justifyContent: 'center', height: 250, marginTop: -25 }}>
+					</View>
+				</View>
+			</View>
+		)
+	}
 	return (
 		<View style={{ backgroundColor: '#151515', flex: 1 }}>
 			<View style={{ flexDirection: 'row', marginTop: 50, justifyContent: 'space-between', alignItems: 'center' }}>
@@ -61,8 +117,7 @@ function PubliChoice(props) {
 			</View>
 
 			<View style={{ alignItems: 'center', paddingTop: 20 }}>
-				<View style={{ alignItems: 'center', }}>
-					<Image source={{ uri: selectedImage.localUri }} style={{ width: 320, height: 240, borderRadius: 20 }} />
+				<View style={{ alignItems: 'center',  width: 320, height: 240, borderRadius: 20, backgroundColor: "grey"  }}>
 				</View>
 			</View>
 
@@ -99,12 +154,12 @@ function PubliChoice(props) {
 
 const styles = StyleSheet.create({
 
-  });
+});
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
 	return {
-		onSelectImage: function (uriFromSelectedImage){
-			dispatch({type: 'imageIsSelected', uri : uriFromSelectedImage})
+		onSelectImage: function (uriFromSelectedImage) {
+			dispatch({ type: 'imageIsSelected', uri: uriFromSelectedImage })
 		}
 	}
 }
